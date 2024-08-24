585f504b0f2faf8165ea30112729a82b2d305088
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-24 16:18:27 +0300

    "Refactor code to remove dummy data, implement post data fetching and static site generation"

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,24 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/Hero";
 import FeaturedPosts from "../components/home-page/FeaturedPosts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with Nextjs",
-    image: "getting-started-with-nextjs.png",
-    excerpt:
-      "NextJs is the the react framework for production - itmakes building fullstack React apps and sites a breeze ships with built-in SSR",
-    date: "2024-08-24",
-  },
-  {
-    slug: "getting-started-with-nextjs-2",
-    title: "Getting started with Nextjs",
-    image: "getting-started-with-nextjs.png",
-    excerpt:
-      "NextJs is the the react framework for production - itmakes building fullstack React apps and sites a breeze ships with built-in SSR",
-    date: "2024-08-24",
-  },
-  {
-    slug: "getting-started-with-nextjs-3",
-    title: "Getting started with Nextjs",
-    image: "getting-started-with-nextjs.png",
-    excerpt:
-      "NextJs is the the react framework for production - itmakes building fullstack React apps and sites a breeze ships with built-in SSR",
-    date: "2024-08-24",
-  },
-  {
-    slug: "getting-started-with-nextjs-4",
-    title: "Getting started with Nextjs ",
-    image: "getting-started-with-nextjs.png",
-    excerpt:
-      "NextJs is the the react framework for production - itmakes building fullstack React apps and sites a breeze ships with built-in SSR",
-    date: "2024-08-24",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
